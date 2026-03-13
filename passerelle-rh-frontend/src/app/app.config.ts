76b65a31fb7c4a6929ad231aac4764bb16799a38
Hanane Oubaha <Hananeouba03@gmail.com>
2026-03-13 16:30:00 +0000

    feat: integration de NgRx Store et Effects pour la gestion globale de la session auth (APP_INITIALIZER + store auth)

--- passerelle-rh-frontend/src/app/app.config.ts
+++ passerelle-rh-frontend/src/app/app.config.ts
@@ -1,16 +1,35 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 import { routes } from './app.routes';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
-
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authFeature } from './store/auth/auth.reducer';
+import { AuthEffects } from './store/auth/auth.effects';
+import { AuthFacade } from './store/auth/auth.facade';
+import { AuthService } from './services/auth.service';
+
+function initAuthSession(authFacade: AuthFacade, authService: AuthService) {
+  return () => {
+    authFacade.hydrateFromStorage(authService.getUser());
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideStore({ [authFeature.name]: authFeature.reducer }),
+    provideEffects([AuthEffects]),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initAuthSession,
+      deps: [AuthFacade, AuthService],
+      multi: true
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
